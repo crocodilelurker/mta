d2857a94f181826a118914dfa24195e2f6589cbb
crocodilelurker <swagatsahu556@gmail.com>
2026-05-26 21:33:36 +0530

    frontend

---
+++ frontend/app/login/page.tsx
@@ -0,0 +1,11 @@
+export default function Login() {
+    return (
+        <div className="flex items-center justify-center h-dvh">
+            <section className="flex-col border-2 rounded-sm p-4 gap-4">
+                <h1>Login</h1>
+                <div></div>
+                <input type="text" placeholder=" email"></input>
+            </section>
+        </div>
+    )
+}
